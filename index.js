115cfc2a568f628bb8b160a18b6e674645228e3c
Fakeha Iftikhar <fakeha.iftikhar@gmail.com>
2024-05-07 19:59:11 -0400

    added a new function that makes request to api to check the number of times satellites are going to fly over the given coords and the duration for which it will be visible

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // index.js
 
 // The code below is temporary and can be commented out.
-const { fetchCoordsByIP } = require('./iss');
+const { fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
-fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+// fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned coordinates:' , coordinates);
+// });
+fetchISSFlyOverTimes({ latitude: '49.27670', longitude: '-123.13000' }, (error, response) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned coordinates:' , coordinates);
+  console.log('It worked! The satellite will Fly over on these times: ', response);
 });
--- iss.js
+++ iss.js
@@ -46,4 +46,19 @@ const fetchCoordsByIP = function(ip, callback) {
  
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
+  needle.get(url, (error, response, body) => {
+    if (error) {
+      return callback(error, null)
+    }
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      return;
+    }
+    const responseFlyOver = body.response;
+    callback(null, responseFlyOver);
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
